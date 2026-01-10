718f300836ddb54f4c349a21660c819067b65d34
huzibaca <masterorigin91@gmail.com>
2026-01-10 21:04:07 +0800

    chore: simplify version suffix to stp

--- constant/version.go
+++ constant/version.go
@@ -2,7 +2,7 @@ package constant
 
 var (
 	Meta       = true
-	Version    = "v1.19.18+shadowtls-plus.0.1.1"
+	Version    = "v1.19.18-stp.0.1.1"
 	BuildTime  = "unknown time"
 	MihomoName = "mihomo"
 )
