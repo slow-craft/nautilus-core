e0b131652119117eef5cef70c2750fde639493a8
huzibaca <masterorigin91@gmail.com>
2026-03-14 01:27:22 +0800

    feat(multi-protocol): add MultiProtocol adapter type constant

--- constant/adapters.go
+++ constant/adapters.go
@@ -47,6 +47,7 @@ const (
 	Sudoku
 	Masque
 	TrustTunnel
+	MultiProtocol
 )
 
 const (
@@ -218,6 +219,8 @@ func (at AdapterType) String() string {
 		return "Masque"
 	case TrustTunnel:
 		return "TrustTunnel"
+	case MultiProtocol:
+		return "MultiProtocol"
 	case Relay:
 		return "Relay"
 	case Selector:
